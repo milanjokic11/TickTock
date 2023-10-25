d1f6cac1f93cdba4fa46661e607d94a8526c5977
milanjokic11 <123904845+milanjokic11@users.noreply.github.com>
2023-10-25 18:02:55 -0500

    initial splash screen
    also adding view binding to program

--- app/src/main/java/eu/tutorials/ticktock/SplashActivity.kt
+++ app/src/main/java/eu/tutorials/ticktock/SplashActivity.kt
@@ -1,11 +1,23 @@
 package eu.tutorials.ticktock
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import eu.tutorials.ticktock.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private var binding: ActivitySplashBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+
+        binding?.tvAppName?.typeface = typeFace
     }
 }
