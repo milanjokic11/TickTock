2ecc1592e1a95c235a573e2a34abee7e7c3258a2
milanjokic11 <123904845+milanjokic11@users.noreply.github.com>
2023-11-09 17:23:47 -0600

    setting up for notifications

--- app/src/main/java/eu/tutorials/ticktock/utils/Constants.kt
+++ app/src/main/java/eu/tutorials/ticktock/utils/Constants.kt
@@ -29,6 +29,14 @@ object Constants {
     const val TICKTOCK_PREFERENCES: String = "TickTock_Preferences"
     const val FCM_TOKEN_UPDATED: String = "fcmTokenUpdated"
     const val FCM_TOKEN: String = "fcmToken"
+    const val FCM_BASE_URL:String = "https://fcm.googleapis.com/fcm/send"
+    const val FCM_AUTHORIZATION:String = "authorization"
+    const val FCM_KEY:String = "key"
+    const val FCM_SERVER_KEY:String = "AAAA7-eGZbI:APA91bHKr76Gkxi4gFtqDqkqz_Td7Adgq2jekYZ3B5R_YJ27hbEvFuzWo8GqLMZ6PQyrU0OjvQYN5GF-3Bw1s038YYIWI7gg8QYQtP-gaBSY7d06J8WEqIf0usRCa0wv9lZyJpU7_2VI"
+    const val FCM_KEY_TITLE:String = "title"
+    const val FCM_KEY_MESSAGE:String = "message"
+    const val FCM_KEY_DATA:String = "data"
+    const val FCM_KEY_TO:String = "to"
 
     fun showImageChooser(activity: Activity) {
         var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
